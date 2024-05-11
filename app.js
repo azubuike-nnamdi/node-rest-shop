eb95c1e0e244dd7c06129e6c042e46135bb1836f
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-05-11 18:42:27 +0100

    chore: install and config nodemon for server loading and morgan for logging error :package:

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
@@ -12,7 +13,26 @@ const orderRoutes = require('./api/routes/orders')
 // })
 // })
 
+app.use(morgan('dev'));
+
+//Routes which should handle requests
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
+
+//Handle request that gets here or routes that don't exist
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = (404)
+  next(error);
+})
+
+//handles general error in the application
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 module.exports = app;

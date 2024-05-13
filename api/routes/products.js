5d8f472a5d9f2afdc558ad8cc66002fc6d2c6814
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-05-13 10:32:01 +0100

    chore: update product http queries :green_heart:

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,46 +1,104 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/products');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Products fetched successfully'
-  });
+  Product.find()
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: 'Products fetched successfully',
+        result
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    });
 });
 
 router.post('/', (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  }
-  res.status(201).json({
-    message: 'Products posted successfully',
-    createdProduct: product
-  });
+  })
+  product.save()
+    .then((result) => {
+      console.log(result)
+      res.status(201).json({
+        message: 'Products created successfully',
+        createdProduct: result
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({
+        error: error
+      })
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: 'Product with special ID already successfully fetched',
-      id: id
-    });
-  } else {
-    res.status(200).json({
-      message: 'Product ID specified'
+  Product.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: 'No valid data found for this product with id'
+        })
+      }
     })
-  }
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Product updated'
+  const id = req.params.productId;
+  const updateOps = {}
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.updateOne({ _id: id }, {
+    $set: updateOps
   })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Product deleted successfully'
-  })
+  const id = req.params.productId;
+  Product.deleteOne({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: 'Product deleted successfully',
+        result
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ err: err });
+    });
 })
 module.exports = router;
